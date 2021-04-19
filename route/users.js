f2cef713c6fead3348632d15feeabee46ffedef3
Gopal Sharma <gopal.sharma@celebaltech.com>
2021-04-19 21:04:53 +0530

    changes in user update api

--- route/users.js
+++ route/users.js
@@ -88,7 +88,7 @@ exports.userSave = function (req, res) {
 
 exports.userUpdate = async function (req, res) {
     const { _id, first_name, last_name, email, phoneno } = req.body;
-    console.log("REQUEST>>>>>>>>>>>>",req.body);
+    //console.log("REQUEST>>>>>>>>>>>>",req.body);
     try {
         const checkData = await User.findOne({ _id });
         if (!checkData) {
@@ -105,37 +105,36 @@ exports.userUpdate = async function (req, res) {
         }
 
         // Save base64ImageUrl
-    if (req.body.base64image == ""){
-        var base64ImageName = checkData.image;;
-    } else {
-        var matches = req.body.base64image.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
-        response = {};
-        
-        if (matches.length !== 3) {
-            return res.status(404).send({'err' : 'Invalid input string.'});
+        if (req.body.base64image == ""){
+            var base64ImageName = checkData.image;;
+        } else {
+            var matches = req.body.base64image.match(/^data:([A-Za-z-+/]+);base64,(.+)$/),
+            response = {};
+            
+            if (matches.length !== 3) {
+                return res.status(404).send({'err' : 'Invalid input string.'});
+            }
+            
+            response.type = matches[1];
+            response.data = new Buffer.from(matches[2], 'base64');
+            let decodedImg = response;
+            console.log("decodedImg>>>>>>>>",decodedImg);
+            let imageBuffer = decodedImg.data;
+            console.log("imageBuffer>>>>>",imageBuffer);
+            let type = decodedImg.type;
+            console.log("type>>>>>>>>>",type);
+            let extension = mime.getExtension(type);
+            var base64ImageName = "image" + '-' + Date.now() + '.' + extension;
+            console.log("base64ImageName>>>>>>>>",base64ImageName);
+            console.log("fileName>>>>>>>>>",fileName);
+            try {
+            fs.writeFileSync("./public/userImages/" + base64ImageName, imageBuffer, 'utf8');
+                //return res.send({"status":"success"});
+            } catch (e) {
+                console.log("e>>>>>>>>>>>>>>>",e);
+                //next(e);
+            }
         }
-        
-        response.type = matches[1];
-        response.data = new Buffer.from(matches[2], 'base64');
-        //Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from()
-        let decodedImg = response;
-        console.log("decodedImg>>>>>>>>",decodedImg);
-        let imageBuffer = decodedImg.data;
-        console.log("imageBuffer>>>>>",imageBuffer);
-        let type = decodedImg.type;
-        console.log("type>>>>>>>>>",type);
-        let extension = mime.getExtension(type);
-        let base64ImageName = "image" + '-' + Date.now() + '.' + extension;
-        console.log("base64ImageName>>>>>>>>",base64ImageName);
-        console.log("fileName>>>>>>>>>",fileName);
-        try {
-        fs.writeFileSync("./public/userImages/" + base64ImageName, imageBuffer, 'utf8');
-            //return res.send({"status":"success"});
-        } catch (e) {
-            console.log("e>>>>>>>>>>>>>>>",e);
-            //next(e);
-        }
-    }
 
         checkData.first_name = first_name;
         checkData.last_name = last_name;
@@ -143,7 +142,7 @@ exports.userUpdate = async function (req, res) {
         checkData.phoneno = phoneno;
         checkData.image = base64ImageName;
         console.log("checkData>>>>>>>",checkData);
-        return false;
+        //return false;
         checkData.save();
         return res.status(200).send({ confirm : 'User has been updated successfully.' });
     } catch (error) {
